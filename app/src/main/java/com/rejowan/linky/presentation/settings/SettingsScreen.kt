4f63c7b71f37d32f184d7d00e901ed0ce8f0e8aa
K M Rejowan Ahmmed <kmrejowan@gmail.com>
2025-10-18 01:16:32 +0600

    refactor CollectionsScreen and SettingsScreen to remove unused imports

--- app/src/main/java/com/rejowan/linky/presentation/settings/SettingsScreen.kt
+++ app/src/main/java/com/rejowan/linky/presentation/settings/SettingsScreen.kt
@@ -19,11 +19,9 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SegmentedButton
 import androidx.compose.material3.SegmentedButtonDefaults
 import androidx.compose.material3.SingleChoiceSegmentedButtonRow
-import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
